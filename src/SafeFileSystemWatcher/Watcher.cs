c0db5d86bccf193da4f7fa072aa049133166a31f
Victor Procure <victor@victorprocure.co>
2019-06-24 13:03:24 -0600

    Added watcher to simply usage

---
+++ src/SafeFileSystemWatcher/Watcher.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using SafeFileSystemWatcher.Configurations;
+
+namespace SafeFileSystemWatcher
+{
+    /// <summary>
+    /// Background threadsafe watcher for <see cref="FileSystemEventArgs"/>
+    /// </summary>
+    public class Watcher
+    {
+        private readonly Action<FileSystemEventArgs> _callback;
+        private readonly CancellationToken _cancellationToken;
+        private readonly FileSystemEventCollectionConfiguration _configuration;
+        private Thread _internalThread;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Watcher"/> class.
+        /// </summary>
+        /// <param name="callback">Callback to execute on new file system event</param>
+        /// <param name="configuration">Initial configuration object</param>
+        /// <param name="cancellationToken">Cancellation token to signal to stop watching</param>
+        public Watcher(Action<FileSystemEventArgs> callback, FileSystemEventCollectionConfiguration configuration,
+            CancellationToken cancellationToken)
+            : this(callback, new DefaultFileSystemEventCollectionConfigurationBuilder(), configuration, cancellationToken)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Watcher"/> class.
+        /// </summary>
+        /// <param name="callback">Callback to execute on new file system event</param>
+        /// <param name="configurationBuilder">Configuration builder to use</param>
+        /// <param name="configuration">Initial configuration object</param>
+        /// <param name="cancellationToken">Cancellation token to signal to stop watching</param>
+        public Watcher(Action<FileSystemEventArgs> callback, IFileSystemEventCollectionConfigurationBuilder configurationBuilder,
+            FileSystemEventCollectionConfiguration configuration, CancellationToken cancellationToken)
+        {
+            _callback = callback;
+            _cancellationToken = cancellationToken;
+            _configuration = configurationBuilder.Build(configuration);
+
+            Initialize();
+        }
+
+        /// <summary>
+        /// Begin monitoring directory for file changes
+        /// </summary>
+        public void Watch()
+            => _internalThread.Start();
+
+        private void Initialize()
+            => _internalThread = new Thread(StartCollectionWatcher);
+
+        private void StartCollectionWatcher()
+        {
+            var collection = new FileSystemEventCollection(_configuration, _cancellationToken);
+            var collectionEnumerator = collection.GetEnumerator();
+            while (collectionEnumerator.MoveNext())
+            {
+                _callback(collectionEnumerator.Current);
+            }
+        }
+    }
+}
---
+++ tests/Tests.SafeFileSystemWatcher/WatcherTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using SafeFileSystemWatcher;
+using SafeFileSystemWatcher.Configurations;
+using Xunit;
+
+namespace Tests.SafeFileSystemWatcher
+{
+    public class WatcherTests
+    {
+        private static readonly string _tempFileExtension = Guid.NewGuid().ToString("N");
+
+        [Fact]
+        public void GivenFileSystemEventWatcherShouldExecuteCallback()
+        {
+            var changedFiles = new List<string>();
+            var config = new FileSystemEventCollectionConfiguration(Path.GetTempPath(), $"*.{_tempFileExtension}");
+
+            using (var cts = new CancellationTokenSource())
+            {
+                var watcher = new Watcher(InternalChanger, config, cts.Token);
+                watcher.Watch();
+
+                var tempFile = CreateTempFile();
+                Thread.Sleep(2000);
+                TryDeleteFile(tempFile);
+                cts.Cancel();
+                Assert.Contains(changedFiles, c => c == tempFile);
+            }
+
+            void InternalChanger(FileSystemEventArgs fse)
+            {
+                lock (changedFiles)
+                {
+                    changedFiles.Add(fse.FullPath);
+                }
+            }
+        }
+
+        private static string CreateTempFile()
+        {
+            var tempFilePath = Path.Combine(Path.GetTempPath(), $"{Guid.NewGuid()}.{_tempFileExtension}");
+            using (File.Create(tempFilePath))
+            {
+                return tempFilePath;
+            }
+        }
+
+        private static void TryDeleteFile(string filePath)
+        {
+            try
+            {
+                File.Delete(filePath);
+            }
+            catch
+            {
+                // Don't throw
+            }
+        }
+    }
+}
